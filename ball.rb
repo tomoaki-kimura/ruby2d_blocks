e67a26f99e04c9a17bab4096623fa8f3afc8a5f4
hearthside-pgm-school <hearthside.pgm@gmail.com>
2024-12-21 18:58:09 +0900

    add ball.move

--- ball.rb
+++ ball.rb
@@ -1,6 +1,34 @@
 class Ball < Circle
+  attr_accessor :is_move, :x_flug, :y_flug, :speed
+
   def initialize
     super
     self.radius = 10
+    self.is_move = false
+    self.x_flug = true
+    self.y_flug = false
+    self.speed = 5
+  end
+
+  def follow(bar)
+    self.x = bar.x + bar.width - radius * 2
+    self.y = bar.y - radius
+  end
+
+  def move
+    if is_move
+      move_(:x)
+      move_(:y)
+    end
+  end
+
+  private
+
+  def move_(axis)
+    if send("#{axis.to_s}_flug")
+      eval("self.#{axis.to_s} += speed")
+    else
+      eval("self.#{axis.to_s} -= speed")
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -2,9 +2,21 @@ require './config'
 
 bar = Bar.new
 ball = Ball.new
+ball.follow(bar)
+
+update do
+  ball.move
+end
 
 on :key do |event|
   bar.move(event)
+  ball.follow(bar) unless ball.is_move
+end
+
+on :key_down do |event|
+  if event.key == "space" && !ball.is_move
+    ball.is_move = true
+  end
 end
 
 show
